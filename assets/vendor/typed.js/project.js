4669550bc985e5024ff33c0b0ed0f6b8b455095b
KCui0327 <kenny.cui@mail.utoronto.ca>
2024-09-20 13:22:16 -0400

    Fixed issue with date sorting

--- assets/vendor/typed.js/project.js
+++ assets/vendor/typed.js/project.js
@@ -1,72 +1,75 @@
 var projects = [
-    ['ECE 297 Mapping Software', [
-        'https://kcui0327.github.io/PRA1-ECE444/assets/img/ece297.png',
-        'Mapping Software for ECE297 Course',
-        ['C++', 'OpenStreetMap (OSM)'],
-        'https://github.com/KCui0327/ece297-hashmapper-showcase',
-        '2023-04'
+    ["ECE 297 Mapping Software", [
+        "https://kcui0327.github.io/PRA1-ECE444/assets/img/ece297.png",
+        "Mapping Software for ECE297 Course",
+        ["C++", "OpenStreetMap (OSM)"],
+        "https://github.com/KCui0327/ece297-hashmapper-showcase",
+        "2023-04-30"
         ]
     ],
-    ['Picea', [
-        'https://kcui0327.github.io/PRA1-ECE444/assets/img/mental_health.png',
-        'A mental health conferencing web app',
-        ['JavaScript', 'AWS', 'axios', 'Serverless Framework'],
-        'https://github.com/KCui0327/Picea',
-        '2024-02'
+    ["Picea", [
+        "https://kcui0327.github.io/PRA1-ECE444/assets/img/mental_health.png",
+        "A mental health conferencing web app",
+        ["JavaScript", "AWS", "axios", "Serverless Framework"],
+        "https://github.com/KCui0327/Picea",
+        "2024-02-21"
         ]
     ],
-    ['Branch', [
-        'https://kcui0327.github.io/PRA1-ECE444/assets/img/branch.png',
-        'A web app for tutoring and mentorship',
-        ['Python', 'Flask', 'axios', 'JavaScript', 'TypeScript', 'CSS', 'axios'],
-        'https://github.com/KCui0327/Branch',
-        '2024-10'
+    ["Branch", [
+        "https://kcui0327.github.io/PRA1-ECE444/assets/img/branch.png",
+        "A web app for tutoring and mentorship",
+        ["Python", "Flask", "axios", "JavaScript", "TypeScript", "CSS", "axios"],
+        "https://github.com/KCui0327/Branch",
+        "2023-10-21"
         ]
     ],
-    ['contentsift', [
-        'https://kcui0327.github.io/PRA1-ECE444/assets/img/contentsift.png',
-        'Web extension to filter out fake news in X (formerly Twitter)',
-        ['JavaScript', 'React', 'Python', 'Microsoft Azure', 'axios'],
-        'https://github.com/KCui0327/contentsift',
-        '2024-03'
+    ["contentsift", [
+        "https://kcui0327.github.io/PRA1-ECE444/assets/img/contentsift.png",
+        "Web extension to filter out fake news in X (formerly Twitter)",
+        ["JavaScript", "React", "Python", "Microsoft Azure", "axios"],
+        "https://github.com/KCui0327/contentsift",
+        "2024-03-10"
         ]
     ],
 ];
 
 var projects_late = [
-    ['shakesafe', [
-        'https://kcui0327.github.io/PRA1-ECE444/assets/img/shakesafe.png',
-        'Andriod App and rescue device for rescue efforts in earthquakes',
-        ['Kotlin', 'C++', 'Arduino', 'OpenAI GPT 3 API', 'Google Maps API', 'Dropbox API'],
-        'https://github.com/KCui0327/shakesafe',
-        '2023-03'
+    ["shakesafe", [
+        "https://kcui0327.github.io/PRA1-ECE444/assets/img/shakesafe.png",
+        "Andriod App and rescue device for rescue efforts in earthquakes",
+        ["Kotlin", "C++", "Arduino", "OpenAI GPT 3 API", "Google Maps API", "Dropbox API"],
+        "https://github.com/KCui0327/shakesafe",
+        "2023-03-06"
         ]
     ],
-    ['forgetful', [
-        'https://kcui0327.github.io/PRA1-ECE444/assets/img/forgetful.png',
-        'A to-do list',
-        ['Python', 'SQLite'],
-        'https://github.com/KCui0327/forgetful',
-        '2022-12'
+    ["forgetful", [
+        "https://kcui0327.github.io/PRA1-ECE444/assets/img/forgetful.png",
+        "A to-do list",
+        ["Python", "SQLite"],
+        "https://github.com/KCui0327/forgetful",
+        "2022-12-12"
         ]
     ]
 ]
 
 var i = 1;
 
+function sortProjects() {
+    projects.sort((a,b) => {
+        const Date1 = new Date(a[1][4]);
+        const Date2 = new Date(b[1][4]);
+        return Date2-Date1
+    });
+}
+
 function loadMore() {
-    document.getElementById('load_button').style.visibility = 'hidden';
+    document.getElementById("load_button").style.visibility = "hidden";
     projects = projects.concat(projects_late);
+    sortProjects();
 }
 
 function rotateProjects() {
     var len = projects.length;
-    projects.sort((a,b) => {
-        const date1 = new Date(a[1][4]);
-        const date2 = new Date(b[1][4]);
-    
-        return date2 - date1;
-    });
     document.getElementById("title1").innerHTML = projects[(i + 0) % len][0];
     document.getElementById("pic1").src = projects[(i + 0) % len][1][0];
     document.getElementById("description1").innerHTML = projects[(i + 0) % len][1][1];
@@ -74,9 +77,9 @@ function rotateProjects() {
     document.getElementById("date1").innerHTML = projects[(i + 0) % len][1][4];
 
     var tools = document.getElementById("tools1")
-    tools.innerHTML = ''
+    tools.innerHTML = ""
     projects[(i + 0) % len][1][2].forEach(function(tool) {
-        var list = document.createElement('li');
+        var list = document.createElement("li");
         list.innerHTML = tool;
         tools.append(list);
     });
@@ -88,9 +91,9 @@ function rotateProjects() {
     document.getElementById("date2").innerHTML = projects[(i + 1) % len][1][4];
 
     var tools = document.getElementById("tools2")
-    tools.innerHTML = ''
+    tools.innerHTML = ""
     projects[(i + 1) % len][1][2].forEach(function(tool) {
-        var list = document.createElement('li');
+        var list = document.createElement("li");
         list.innerHTML = tool;
         tools.append(list);
     });
@@ -103,9 +106,9 @@ function rotateProjects() {
 
 
     var tools = document.getElementById("tools3")
-    tools.innerHTML = ''
+    tools.innerHTML = ""
     projects[(i + 2) % len][1][2].forEach(function(tool) {
-        var list = document.createElement('li');
+        var list = document.createElement("li");
         list.innerHTML = tool;
         tools.append(list);
     });
@@ -113,6 +116,7 @@ function rotateProjects() {
     i++;
 }
 
+sortProjects();
 rotateProjects();
 
 setInterval(rotateProjects, 2000);
